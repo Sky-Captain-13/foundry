8645e949aa2a7d4aa0f2b49ea95b9de2c3201679
Sky <Sky-Captain-13@users.noreply.github.com>
2019-09-17 07:58:56 -0400

    Update to v0.0.2
    - Adds tool, weapon, and armor proficiencies to the character sheet
    - Fixes some css conflicts
    - Adds scrollbar position saving (big shout out to Moerill for this one)

--- sky5e/sky5e/sky5e.js
+++ sky5e/sky5e/sky5e.js
@@ -1,20 +1,56 @@
 // @author Sky#9453
-// @version 0.0.1
+// @version 0.0.2
 // 
-// Special Thanks to DM Me Your Waifu (Discord: Fyorl#1292) for their
-// assistance in getting the additional proficiencies to work properly.
+// Special Thanks:
+//	- DM Me Your Waifu (Discord: Fyorl#1292) for their assistance in getting the additional proficiencies to work properly.
+//  - Kakaroto for helping with clickable known languages to chat
+//	- Moerill for providing the Scrollbar Position saving code and dealing with my idiocy
 
 CONFIG.tools = {
-	"Artisan's Tools": "Artisan's Tools",
-	"Disguise Kit": "Disguise Kit",
-	"Forgery Kit": "Forgery Kit",
-	"Herbalism Kit": "Herbalism Kit",
-	"Navigator's Tools": "Navigator's Tools",
-	"Poisoner's Kit": "Poisoner's Kit",
-	"Thieves' Tools": "Thieves' Tools",
-	"Vehicles (Air)": "Vehicles (Air)",
-	"Vehicles (Land)": "Vehicles (Land)",
-	"Vehicles (Water)": "Vehicles (Water)"
+	"Artisan's Tool": "ARTISAN'S TOOLS",
+	"Alchemist's Supplies": " - Alchemist's Supplies",
+	"Brewer's Supplies	": " - Brewer's Supplies",
+	"Calligrapher's Supplies	": " - Calligrapher's supplies",
+	"Carpenter's Tools	": " - Carpenter's Tools",
+	"Cartographer's Tools	": " - Cartographer's Tools",
+	"Cobbler's Tools	": " - Cobbler's Tools",
+	"Cook's Utensils	": " - Cook's Utensils",
+	"Glassblower's Tools	": " - Glassblower's Tools",
+	"Jeweler's Tools	": " - Jeweler's Tools",
+	"Leatherworker's Tools	": " - Leatherworker's Tools",
+	"Mason's Tools	": " - Mason's Tools",
+	"Painter's supplies	": " - Painter's supplies",
+	"Potter's Tools	": " - Potter's Tools",
+	"Smith's Tools	": " - Smith's Tools",
+	"Tinker's Tools	": " - Tinker's Tools",
+	"Weaver's Tools	": " - Weaver's Tools",
+	"Woodcarver's Tools	": " - Woodcarver's Tools",
+	"Gaming Sets": "GAMING SETS",
+	"Dice": " - Dice",
+	"Dragonchess": " - Dragonchess",
+	"Playing Cards": " - Playing Cards",
+	"Three-Dragon Ante": " - Three Dragon Ante",
+	"Musical Instrument": "MUSICAL INSTRUMENTS",
+	"Bagpipes": " - Bagpipes",
+	"Drum": " - Drum",
+	"Dulcimer": " - Dulcimer",
+	"Flute": " - Flute",
+	"Lute": " - Lute",
+	"Lyre	": " - Lyre	",
+	"Horn": " - Horn",
+	"Pan Flute": " - Pan Flute",
+	"Shawm": " - Shawm",
+	"Viol": " - Viol",
+	"Other Tools": "OTHER TOOLS & KITS",
+	"Disguise Kit": " - Disguise Kit",
+	"Forgery Kit": " - Forgery Kit",
+	"Herbalism Kit": " - Herbalism Kit",
+	"Navigator's Tools": " - Navigator's Tools",
+	"Poisoner's Kit": " - Poisoner's Kit",
+	"Thieves' Tools": " - Thieves' Tools",
+	"Vehicles (Air)": " - Vehicles (Air)",
+	"Vehicles (Land)": " - Vehicles (Land)",
+	"Vehicles (Water)": " - Vehicles (Water)"
 };
 
 CONFIG.armor = {
@@ -23,7 +59,7 @@ CONFIG.armor = {
 	"Medium": "Medium",
 	"Heavy": "Heavy",
 	"Shields": "Shields"
-}
+};
 
 CONFIG.weapons = {
 	"Simple": "Simple",
@@ -66,7 +102,7 @@ CONFIG.weapons = {
 	"Warhammer": "Warhammer",
 	"Whip": "Whip",
 	"Yklwa": "Yklwa"
-}
+};
 
 class Sky5eSheet extends ActorSheet5eCharacter {
 	get template() {
@@ -78,32 +114,39 @@ class Sky5eSheet extends ActorSheet5eCharacter {
 	static get defaultOptions() {
 		const options = super.defaultOptions;
 		options.classes.push('sky5e');
-		mergeObject(options, {
-			width: 900,
-			height: 800
-		});
+		mergeObject(options, {width: 900, height: 800});
 		return options;
+	}
+	
+	async _render(force = false, options = {}) {
+		this.saveScrollPos();
+		await super._render(force, options);
+		this.setScrollPos();
+	}
+
+	saveScrollPos() {
+		if (this.form === null) return;
+		const html = $(this.form).parent();
+		this.scrollPos = {
+			top: html.scrollTop(),
+			left: html.scrollLeft()
+		}
+	}
+
+	setScrollPos() {
+		if (this.form === null || this.scrollPos === undefined) return;
+		const html = $(this.form).parent();
+		html.scrollTop(this.scrollPos.top);
+		html.scrollLeft(this.scrollPos.left);
 	}
 	
 	getData() {
 		const data = super.getData();
 		if (data.actor.flags.sky5e === undefined) {
 			const flags = {
-				tools: {
-					custom: "",
-					label: "Tools",
-					value: []
-				},
-				armor: {
-					custom: "",
-					label: "Armor",
-					value: []
-				},
-				weapons: {
-					custom: "",
-					label: "Weapons",
-					value: []
-				}
+				Tools: { custom: "", label: "Tools", value: [] },
+				armor: { custom: "", label: "Armor", value: [] },
+				weapons: { custom: "", label: "Weapons", value: [] }
 			};
 			data.actor.flags.sky5e = flags;
 			this.actor.update({'flags.sky5e': flags});
@@ -132,39 +175,61 @@ class Sky5eSheet extends ActorSheet5eCharacter {
 					},
 					cancel: {
 						icon: '<i class="fas fa-times"></i>',
-						label: 'Cancel'
+						label: 'No'
 					},
 				},
-				default: 'Yes'
+				default: 'cancel'
 			}).render(true);
 		});
+		
+		// Ability Checks
+    html.find('.ability-mod').click(event => {
+      event.preventDefault();
+      let ability = event.currentTarget.parentElement.getAttribute("data-ability");
+      this.actor.rollAbilityTest(ability, {event: event});
+    });
+		
+		// Ability Saves
+    html.find('.ability-save').click(event => {
+      event.preventDefault();
+      let ability = event.currentTarget.parentElement.getAttribute("data-ability");
+      this.actor.rollAbilitySave(ability, {event: event});
+    });
+		
+		// Send Languages to Chat
+    html.find('.sky-languages').click(event => {
+      event.preventDefault();
+			let langs = this.actor.data.data.traits.languages.value.map(l => CONFIG.languages[l] || l).join(", ");
+			let content = `
+				<div class="dnd5e chat-card item-card" data-acor-id="${this.actor._id}">
+					<header class="card-header flexrow">
+						<img src="${this.actor.data.token.img}" title="" width="36" height="36" style="border: none;"/>
+						<h3>Known Languages</h3>
+					</header>
+					<div class="card-content">${langs}</div>
+				</div>`;
+			
+			// Send content to chat
+			let rollWhisper = null;
+			let rollBlind = false;			
+			let	rollMode = game.settings.get("core", "rollMode");
+			if (["gmroll", "blindroll"].includes(rollMode)) rollWhisper = ChatMessage.getWhisperIDs("GM");
+			if (rollMode === "blindroll") rollBlind = true;
+			
+			ChatMessage.create({
+				user: game.user._id,
+				content: content,
+				speaker: { actor: this.actor._id, token: this.actor.token, alias: this.actor.name },
+				type: CHAT_MESSAGE_TYPES.OTHER,
+				whisper: rollWhisper,
+				blind: rollBlind,
+				sound: CONFIG.sounds.dice
+			});
+    });	
 	}
 }
 
 Actors.registerSheet("dnd5e", Sky5eSheet, {
 	types: ["character"],
 	makeDefault: true
-});
-
-Hooks.on('preCreateActor', (cls, data) => {
-		if (data.flags === undefined) {
-				data.flags = {};
-		}
-		data.flags.sky5e = {
-			tools: {
-				custom: "",
-				label: "Tools",
-				value: []
-			},
-			armor: {
-				custom: "",
-				label: "Armor",
-				value: []
-			},
-			weapons: {
-				custom: "",
-				label: "Weapons",
-				value: []
-			}
-		};
 });
