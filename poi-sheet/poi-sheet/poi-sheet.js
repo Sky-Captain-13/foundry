2b4b76eadc1ccc59f086508c8fc86ec283d447b1
Sky <Sky-Captain-13@users.noreply.github.com>
2020-01-07 22:28:48 -0500

    Initial commit

---
+++ poi-sheet/poi-sheet/poi-sheet.js
@@ -0,0 +1,21 @@
+// @author Sky#9453
+// @version 0.0.1
+
+class PoISheet extends ActorSheet {
+	get template() {
+		if ( !game.user.isGM && this.actor.limited ) return super.template;
+		return "modules/poi-sheet/poi-sheet.html";
+	}
+	
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('dnd5e sheet actor character');
+		mergeObject(options, {width: 672, height: 736});
+		return options;
+	}
+}
+
+Actors.registerSheet("dnd5e", PoISheet, {
+	types: ["character"],
+	makeDefault: false
+});
