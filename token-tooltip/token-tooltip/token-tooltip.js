b9291cdfeab2531e0607cb0d064df0d2d2422037
Sky <Sky-Captain-13@users.noreply.github.com>
2020-01-11 01:05:01 -0500

    Formatting fix.

--- token-tooltip/token-tooltip/token-tooltip.js
+++ token-tooltip/token-tooltip/token-tooltip.js
@@ -2,34 +2,34 @@
 Hooks.on("hoverToken", (object, hovered) => {
 	if (!object || !object.actor) return;
 	if (event.altKey) return;
-	
+
 	// SETTINGS & DECLARATIONS
 	let showTooltip = game.settings.get("token-tooltip", "tooltipVisibility");
 	let disposition = parseInt(object.data.disposition);
-	
+
 	// PARSE TOKEN/ACTOR INFO
 	let info = null;
-  try {
-    info = {
-      ac: isNaN(parseInt(object.actor.data.data.attributes.ac.value)) ? 10 : parseInt(object.actor.data.data.attributes.ac.value),
+	try {
+		info = {
+			ac: isNaN(parseInt(object.actor.data.data.attributes.ac.value)) ? 10 : parseInt(object.actor.data.data.attributes.ac.value),
 			hp: parseInt(object.actor.data.data.attributes.hp.value),
 			temphp: parseInt(object.actor.data.data.attributes.hp.temp),
 			hpmax: parseInt(object.actor.data.data.attributes.hp.max) + parseInt((object.actor.data.data.attributes.hp.tempmax !== "") ? object.actor.data.data.attributes.hp.tempmax : 0),
 			speed: object.actor.data.data.attributes.speed.value,
-      passives: {
-        perception: 10 + parseInt(object.actor.data.data.skills.prc.mod),
-        investigation: 10 + parseInt(object.actor.data.data.skills.inv.mod)
-      }
-    };
+			passives: {
+				perception: 10 + parseInt(object.actor.data.data.skills.prc.mod),
+				investigation: 10 + parseInt(object.actor.data.data.skills.inv.mod)
+			}
+		};
 		// CHECK IF TARGET HAS TEMP HP AND ADD TO TOOLTIP
 		if (info.temphp !== 0) info.hp = info.hp + " (+" + info.temphp + ")";
-  } catch (error) {
-    return;
-  }
-	
+	} catch (error) {
+		return;
+	}
+
 	// TEMPORARY FIX FOR TOOLTIPS ON OVERWORLD MAP
 	if (parseInt(info.ac) === 0) return;
-	
+
 	// DEFINE THE TEMPLATES
 	let fullTemplate = $(`
 		<div class="section">
@@ -46,7 +46,7 @@ Hooks.on("hoverToken", (object, hovered) => {
 			<div class="value"><i class="fas fa-heart"></i>${info.hp} / ${info.hpmax}</div>
 		</div>
     `);
-	
+
 	// DETERMINE WHICH TOOLTIP TO USE
 	let template = "";
 	if (game.user.isGM) {
@@ -74,18 +74,18 @@ Hooks.on("hoverToken", (object, hovered) => {
 			hovered = false;
 		}
 	}
-	
-  // ADD OR REMOVE THE TOOLTIP
-  if (hovered) {
+
+	// ADD OR REMOVE THE TOOLTIP
+	if (hovered) {
 		let dmtktooltip = $(`<div class="dmtk-tooltip ${object.data._id}"></div>`);
 		dmtktooltip.css('left', (event.pageX + 10) + 'px');
-    dmtktooltip.css('top', (event.pageY + 10) + 'px'); 
-    dmtktooltip.html(template);
+		dmtktooltip.css('top', (event.pageY + 10) + 'px');
+		dmtktooltip.html(template);
 		$('body.game').append(dmtktooltip);
-		setTimeout(function() {
+		setTimeout(function () {
 			$("." + object.data._id).remove();
 		}, 5000);
-  } else {
+	} else {
 		$("." + object.data._id).remove();
-  }
+	}
 });
